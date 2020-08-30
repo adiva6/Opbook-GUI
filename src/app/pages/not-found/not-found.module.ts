a8a54f214af67ccb4aa076419a806f549831019e
Adi Weinstock <adi.wein6@gmail.com>
2020-08-30 22:51:05 +0300

    Add not found page

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -3,13 +3,15 @@ import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    NotFoundRoutingModule,
-    RouterModule
-  ],
+    imports: [
+        CommonModule,
+        NotFoundRoutingModule,
+        RouterModule,
+        MatButtonModule
+    ],
   declarations: [
     NotFoundComponent
   ]
